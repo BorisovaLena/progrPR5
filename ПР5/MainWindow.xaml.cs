b69627e1967db42932b33e820a80f650265bc686
elenabor2003@gmail.com <elenabor2003@gmail.com>
2022-09-27 17:19:45 +0300

    commit2

--- ПР5/MainWindow.xaml.cs
+++ ПР5/MainWindow.xaml.cs
@@ -28,12 +28,70 @@ namespace ПР5
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             vvodKmM.Visibility = Visibility.Visible;
-
+            choiceOfUnits.Visibility = Visibility.Visible;
+            choiceOfUnits2.Visibility = Visibility.Collapsed;
+            vvodKol.Visibility = Visibility.Collapsed;
         }
 
         private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
         {
             vvodKmM.Visibility = Visibility.Collapsed;
+            choiceOfUnits.Visibility = Visibility.Collapsed;
+            choiceOfUnits2.Visibility = Visibility.Visible;
+            vvodKol.Visibility = Visibility.Visible;
+
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            double rez;
+            int kolKm = Convert.ToInt32(txtKolKm.Text);
+            int kolM = Convert.ToInt32(txtKolM.Text);
+            switch (cmb1.SelectedIndex)
+            {
+                case 0:
+                    rez = (kolKm * 1000 + kolM) * 0.000621371;
+                    txtRez.Text = rez+ " миля";
+                    break;
+                case 1:
+                    rez = (kolKm * 1000 + kolM) * 3.28084;
+                    txtRez.Text = rez + " фут";
+                    break;
+                case 2:
+                    rez = (kolKm * 1000 + kolM) * 1.09361;
+                    txtRez.Text = rez + " ярд";
+                    break;
+                case 3:
+                    rez = (kolKm * 1000 + kolM) * 39.3701;
+                    txtRez.Text = rez + " дюйм";
+                    break;
+                case 4:
+                    rez = (kolKm * 1000 + kolM) * 0.000937383;
+                    txtRez.Text = rez + " верст";
+                    break;
+            }
+        }
+
+        private void cmb2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch(cmb2.SelectedIndex)
+            {
+                case 0:
+                    units.Text = "м";
+                    break;
+                case 1:
+                    units.Text = "фнт";
+                    break;
+                case 2:
+                    units.Text = "ярд";
+                    break;
+                case 3:
+                    units.Text = "дм";
+                    break;
+                case 4:
+                    units.Text = "врст";
+                    break;
+            }
         }
     }
 }
