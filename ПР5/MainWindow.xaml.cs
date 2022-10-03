dd60ee22a913e07a43a2ecf5ac75edd30d3870b3
Lena_Borisova <elenabor2003@gmail.com>
2022-10-03 21:33:16 +0300

    commit 03.10.22

--- ПР5/MainWindow.xaml.cs
+++ ПР5/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ПР5
 {
@@ -24,89 +13,122 @@ namespace ПР5
         {
             InitializeComponent();
         }
-
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
             oneClick.Visibility = Visibility.Visible;
             twoClick.Visibility = Visibility.Collapsed;
             reztwo.Visibility = Visibility.Collapsed;
             btnEnter.Visibility = Visibility.Visible;
+            cmb2.SelectedIndex = -1;
+            txtKol.Text = "";
         }
-
         private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
         {
             oneClick.Visibility = Visibility.Collapsed;
             twoClick.Visibility = Visibility.Visible;
-            rezone.Visibility = Visibility.Collapsed;           
+            rezone.Visibility = Visibility.Collapsed;
             btnEnter.Visibility = Visibility.Visible;
+            cmb1.SelectedIndex = -1;
+            txtKolKm.Text = "";
+            txtKolM.Text = "";
         }
-
         private void btnEnter_Click(object sender, RoutedEventArgs e)
         {
-            if(one.IsChecked==true)
+            if (one.IsChecked == true)
             {
-                double rez;
-                int kolKm = Convert.ToInt32(txtKolKm.Text);
-                int kolM = Convert.ToInt32(txtKolM.Text);
-                switch (cmb1.SelectedIndex)
+                if (txtKolKm.Text!="" && txtKolM.Text != "")
                 {
-                    case 0:
-                        rez = (kolKm * 1000 + kolM) * 0.000621371;
-                        txtRez.Text = Convert.ToString(kolM);
-                        break;
-                    case 1:
-                        rez = (kolKm * 1000 + kolM) * 3.28084;
-                        txtRez.Text = Convert.ToString(kolM);
-                        break;
-                    case 2:
-                        rez = (kolKm * 1000 + kolM) * 1.09361;
-                        txtRez.Text = Convert.ToString(kolM);
-                        break;
-                    case 3:
-                        rez = (kolKm * 1000 + kolM) * 39.3701;
-                        txtRez.Text = Convert.ToString(kolM);
-                        break;
-                    case 4:
-                        rez = (kolKm * 1000 + kolM) * 0.000937383;
-                        txtRez.Text = Convert.ToString(kolM);
-                        break;
+                    double rez;
+                    int kolKm = Convert.ToInt32(txtKolKm.Text);
+                    int kolM = Convert.ToInt32(txtKolM.Text);
+                    switch (cmb1.SelectedIndex)
+                    {
+                        case 0:
+                            rez = (kolKm * 1000 + kolM) * 0.000621371;
+                            txtRez.Text = Convert.ToString(rez);
+                            showRez();
+                            break;
+                        case 1:
+                            rez = (kolKm * 1000 + kolM) * 3.28084;
+                            txtRez.Text = Convert.ToString(rez);
+                            showRez();
+                            break;
+                        case 2:
+                            rez = (kolKm * 1000 + kolM) * 1.09361;
+                            txtRez.Text = Convert.ToString(rez);
+                            showRez();
+                            break;
+                        case 3:
+                            rez = (kolKm * 1000 + kolM) * 39.3701;
+                            txtRez.Text = Convert.ToString(rez);
+                            showRez();
+                            break;
+                        case 4:
+                            rez = (kolKm * 1000 + kolM) * 0.000937383;
+                            txtRez.Text = Convert.ToString(rez);
+                            showRez();
+                            break;
+                        default:
+                            MessageBox.Show("Выберите из списка единицу измерения, в которую нужно перевести!!!");
+                            break;
+                    }
                 }
-                rezone.Visibility = Visibility.Visible;
-                reztwo.Visibility = Visibility.Collapsed;
+                else
+                {
+                    MessageBox.Show("Введите данные!");
+                }
             }
-            else if(two.IsChecked==true)
+            if (two.IsChecked == true)
             {
-                double kol = Convert.ToDouble(txtKol.Text);
-                
-                double qq;
-                switch(cmb2.SelectedIndex)
+                if (txtKol.Text!="")
                 {
-                    case 0:
-                        qq = kol * 1609.34;
-                        podsch(qq);
-                        break;
-                    case 1:
-                        qq = kol * 0.3048;
-                        podsch(qq);
-                        break;
-                    case 2:
-                        qq = kol * 0.9144;
-                        podsch(qq);
-                        break;
-                    case 3:
-                        qq = kol * 0.0254;
-                        podsch(qq);
-                        break;
-                    case 4:
-                        qq = kol * 1066.8;
-                        podsch(qq);
-                        break;
+                    try
+                    {
+                        double kol = Convert.ToDouble(txtKol.Text);
+                        double qq;
+                        switch (cmb2.SelectedIndex)
+                        {
+                            case 0:
+                                qq = kol * 1609.34;
+                                podsch(qq);
+                                showRez2();
+                                break;
+                            case 1:
+                                qq = kol * 0.3048;
+                                podsch(qq);
+                                showRez2();
+                                break;
+                            case 2:
+                                qq = kol * 0.9144;
+                                podsch(qq);
+                                showRez2();
+                                break;
+                            case 3:
+                                qq = kol * 0.0254;
+                                podsch(qq);
+                                showRez2();
+                                break;
+                            case 4:
+                                qq = kol * 1066.8;
+                                podsch(qq);
+                                showRez2();
+                                break;
+                            default:
+                                MessageBox.Show("Выберите из списка единицу измерения, которую нужно перевести!!!");
+                                break;
+                        }
+                    }
+                    catch(FormatException)
+                    {
+                        MessageBox.Show("Вы ввели данные неправильного формата! Для выделения дробных частей используйте запятую!");
+                    }
                 }
-                reztwo.Visibility = Visibility.Visible;
-                rezone.Visibility = Visibility.Collapsed;
+                else
+                {
+                    MessageBox.Show("Выберите единицу измерения и введите данные!");
+                }
             }
         }
-
         private void podsch(double qq)
         {
             int kolKm = 0;
@@ -120,6 +142,22 @@ namespace ПР5
             RezKm.Text = Convert.ToString(kolKm) + " км";
             RezM.Text = Convert.ToString(kolM) + " м";
         }
-
+        private void txtKolKm_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                e.Handled = true;
+            }
+        }
+        private void showRez()
+        {
+            rezone.Visibility = Visibility.Visible;
+            reztwo.Visibility = Visibility.Collapsed;
+        }
+        private void showRez2()
+        {
+            reztwo.Visibility = Visibility.Visible;
+            rezone.Visibility = Visibility.Collapsed;
+        }
     }
 }
